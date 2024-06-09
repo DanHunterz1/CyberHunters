2073f653f3b87da1d5f3f81587d979db3361bb3d
Danrley <Danrleygja@gmail.com>
2024-06-09 19:13:28 -0300

    15ª vez tentativa

---
+++ src/main/java/com/JavaProject/Mimikyu_project/CyberHunter.java
@@ -0,0 +1,13 @@
+package com.JavaProject.Mimikyu_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberHunter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberHunter.class, args);
+	}
+
+}
